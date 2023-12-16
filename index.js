a4e45fd49cde8a58e10d7c23a7c6b8830ca1a97c
Programing Hero <sajalb1899@gmail.com>
2023-12-16 23:03:02 +0600

    insert data

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 5000;
 
-const app = express()
-require('dotenv').config()
-const port = process.env.PORT || 5000
-
-
-
+//middleware
+app.use(cors());
+app.use(express.json())
 
 
 
@@ -24,21 +25,33 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+
+
+    const formCollection = client.db("libraryDB").collection("form")
+
+    app.post('/form', async (req, res) => {
+      const userForm = req.body;
+      // console.log(userForm)
+      const result = await formCollection.insertOne(userForm);
+      res.send(result)
+  })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-app.get('/', (req,res)=>{
-    res.send('Hello World')
+app.get('/', (req, res) => {
+  res.send('Hello World')
 })
 
-app.listen(port, ()=>{
-    console.log(`Example app listening on port ${port}`)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
