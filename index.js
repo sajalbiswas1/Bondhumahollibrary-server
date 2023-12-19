6d5a7fc910df8f74574142e6879f738a655d0135
Programing Hero <sajalb1899@gmail.com>
2023-12-19 23:38:06 +0600

    some problem solve in mongodb

--- index.js
+++ index.js
@@ -27,8 +27,6 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-
-
     const formCollection = client.db("libraryDB").collection("form")
 
     app.post('/form', async (req, res) => {
